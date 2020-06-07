073b9aa03d7abe14386b2fd177a71c6708c076e1
yograjsk <yograjsk@gmail.com>
2020-06-07 13:02:03 +0530

    iframes, keys, windowtabs

--- pendingTopics/workingWithFrameInsideFrame.py
+++ pendingTopics/workingWithFrameInsideFrame.py
@@ -16,16 +16,13 @@ class handleIframes():
                 self.driver.switch_to.frame(i)
                 iframes2 = self.driver.find_elements_by_tag_name("iframe")
                 print("count of iframes inside this iframe: ", len(iframes2))
-                self.driver.switch_to.frame(i)
+                # self.driver.switch_to.frame(i)
                 for a in iframes2:
                     print(a.get_attribute("src"))
                     if "SingleFrame.html" in a.get_attribute("src"):
                         self.driver.switch_to.frame(a)
                         self.driver.find_element_by_tag_name("input").send_keys("This is the input box inside iframe of an iframe")
-
-
-
-# //div[@class='container']//input
+                        self.driver.switch_to.default_content()
 
 hf = handleIframes()
 hf.handleTheIframe()
